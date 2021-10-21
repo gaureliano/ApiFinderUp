d8bdf4131253439aaf7fe48cafabad7cbca9fb1d
Gabriel Aureliano Buzinaro <gaabriel.aureliano@outlook.com>
2021-10-21 20:58:50 -0300

    Add files via upload

--- prodroutes.js
+++ prodroutes.js
@@ -2,6 +2,8 @@ const express = require('express')
 const mongoose = require('mongoose')
 const prodroutes = express.Router()
 const mysql = require ('./mysql').pool
+var dateTime = require('node-datetime')
+let quantAtual = 0
 
 prodroutes.get('/produtos', (req,res) => {
 
@@ -41,21 +43,71 @@ prodroutes.post('/cadastroProd', (req,res) => {
             [req.body.name,req.body.quantity],
             (error, resultado, field) => {
                 conn.release();
-
                 if (error){
                     return res.status(500).send({
                         error:error,
                         response: null
                     });
                 } 
-
                 res.status(201).send({
                     mensagem: 'Produto Inserido com sucesso',
                     id_produto: resultado.insertId
                 })
             }
-        )
+        )          
+    });
+
+});
+
+prodroutes.patch('/adicionarProd', (req,res) => {
     
+    var dt = dateTime.create();
+    var date = dt.format('Y-m-d H:M:S');
+
+    mysql.getConnection((error, conn) =>{
+        if(error){ return res.status(500).send({ error: error })}
+        conn.query(
+            'SELECT * FROM produto WHERE id = ?;',
+            [req.body.id_prod],
+            (error, resultado, fields) => {
+                if (error) { return res.status(500).send({ error: error })}
+                var string=JSON.stringify(resultado);
+                var json =  JSON.parse(string);
+                let quantAtual = json[0].quant + req.body.quant
+                console.log(quantAtual,json[0].quant)
+            }
+        )
+        console.log(quantAtual)
+        conn.query(
+            `UPDATE produto
+                SET quant = ?
+            WHERE id = ?;`,
+            [quantAtual,req.body.id_prod],
+            (error, resultado, field) => {
+                if (error){
+                    return res.status(500).send({
+                        error:error,
+                        response: null
+                    });
+                } 
+                console.log(quantAtual)
+            }
+        )
+        conn.query(
+            'INSERT INTO transacoes (id_func, id_prod, quant, data) VALUES (?,?,?,?)',
+            [req.body.id_func,req.body.id_prod,req.body.quant,date],
+            (error, resultado, field) => {
+                if (error){
+                    return res.status(500).send({
+                        error:error,
+                        response: null
+                    });
+                } 
+                res.status(201).send({
+                    mensagem: 'Quantidade alterada',
+                })
+            }
+        )          
     });
 
 });
